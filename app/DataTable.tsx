e7fbf3a41fafa295f059e49724de0d57ee5128cf
Cevij <38700628+Cevij@users.noreply.github.com>
2023-09-28 10:44:24 -0500

    table now handles no data

--- app/DataTable.tsx
+++ app/DataTable.tsx
@@ -4,6 +4,7 @@ import {
     Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue
 } from "@nextui-org/react"
 import { Lead } from "./Model/LeadModel"
+import { STATUS } from "./Model/StatusEnum"
 
 //now we are expecting 2 props: columns and the data(that we are expecting from the page component)
 export function DataTable({
@@ -16,13 +17,19 @@ export function DataTable({
             <TableHeader columns={columns}>
                 {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
             </TableHeader>
-            <TableBody items={data}>
+            {data?.length ? <TableBody items={data}>
                 {(item: Lead) => (
                     <TableRow key={item.id}>
-                        {(columnKey: any) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+                        {(columnKey: any) => {
+                            if (columnKey === 'status') return <TableCell>{STATUS[getKeyValue(item, columnKey)]}</TableCell>
+                            return <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+                        }
+                        }
                     </TableRow>
                 )}
-            </TableBody>
+            </TableBody> 
+            : 
+            <TableBody emptyContent={"No rows to display."}>{[]}</TableBody>}
         </Table>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,7 @@ import { STATUS } from "./Model/StatusEnum";
 async function getLeads(): Promise<Lead[]> {
   const res = await fetch('http://localhost:5000/api/leads');
   const leads: Lead[] = await res.json()
-
-  leads.map(lead => {
-    lead.status = STATUS[lead.status]
-  })
-
+  // const leads: Lead[] = [];
   // Fetch data from your API here.
 
   return leads;
